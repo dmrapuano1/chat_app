690947965a03995ee9be413b1955a1861a2984aa
dmrapuano1 <dmrapuano1@gmail.com>
2020-08-12 14:43:38 -0400

    adding connection to firebase

--- components/Chat.js
+++ components/Chat.js
@@ -8,9 +8,59 @@ export default function ChatScreen(props) {
   const [messages, setMessages] = useState([]);
   const [background, setBackground] = useState([]);
   const [userName, setName] = useState([]);
+  const [uid, setID] = useState([]);
+  const [logText, setLogText] = useState([]);
 
-  // IIFE to set props to variables
-  const pullProps = (( () => {
+  // Defines firebase to use as DB
+  const firebase = require('firebase');
+  require('firebase/firestore');
+
+  if (!firebase.apps.length){
+    firebase.initializeApp({
+      apiKey: "AIzaSyDoYIcYbogi7UxNPtI5B_Umyr3yaHYUgLo",
+      authDomain: "chat-app-854ee.firebaseapp.com",
+      databaseURL: "https://chat-app-854ee.firebaseio.com",
+      projectId: "chat-app-854ee",
+      storageBucket: "chat-app-854ee.appspot.com",
+      messagingSenderId: "1056389705300"
+    });
+  }
+
+  function onChatUpdate (querySnapshot) {
+    console.log('///////////////// In on chat update /////////////////');
+    const list = [];
+    // go through each document
+    if (querySnapshot) {
+      querySnapshot.forEach((doc) => {
+        // get the QueryDocumentSnapshot's data
+        var data = doc.data();
+        list.push({
+          _id: list.length,
+          text: data.body.text,
+          createdAt: data.body.createdAt,
+          user: {
+            _id: data.body.userID,
+            name: data.body.userName,
+            avatar: 'https://placeimg.com/140/140/any'
+          }
+        });
+      });
+      console.log(messages)
+    if (messages.length !== list.length) setMessages(list)
+    }
+  };
+
+  // Defines and pulls collection 'chat' from firebase 
+  const chatLog = firebase.firestore().collection('chat');
+  
+  // Updates view to display current chat log
+  function unsubscribe() {
+    chatLog.onSnapshot(onChatUpdate)
+  }
+  
+  unsubscribe();
+
+  useEffect(() => {
     
     // pulls props from previous screen
     let { name, color } = props.route.params;
@@ -27,44 +77,64 @@ export default function ChatScreen(props) {
     // sets top navbar to have title of the user's entered name
     props.navigation.setOptions({ title: userName });
 
-  }))();
+    const authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: `Hello ${userName}, start sending messages by typing in the field below`,
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-      {
-       _id: 2,
-       text: `Welcome ${userName}, you have entered a chat`,
-       createdAt: new Date(),
-       system: true,
-      },
-    ])
-  }, [])
+      // If user is not already signed in, creates anonymous user ID
+      if (!uid) {
+      console.log('Working')
+        await firebase.auth().signInAnonymously();
+        // set state of userID
+        setID(user.uid)
+      }
+  
+      // set state of welcome text, unique to this user
+      setLogText(`Welcome ${userName}, you have entered a chat`)
+    });
+
+    authUnsubscribe();
+  }, [props.route.params]);
+
+  // useEffect(() => {
+  //   setMessages([
+  //     {
+  //       _id: 1,
+  //       text: `Hello ${userName}, start sending messages by typing in the field below`,
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 2,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //     {
+  //      _id: 2,
+  //      text: logText,
+  //      createdAt: new Date(),
+  //      system: true,
+  //     },
+  //   ])
+  // }, [])
 
   const onSend = useCallback((messages = []) => {
+    let body = messages[0]
+    chatLog.add({body})
+    console.log('messages', messages[0])
     setMessages(previousMessages => 
       GiftedChat.append(previousMessages, messages))
   }, [])
 
   return (
+    
     // styling background to have color and cover the entire screen
     <View style={{flex:1, backgroundColor: background}}>
 
       {/* using react-native-gifted-chat to display and update messages in view */}
       <GiftedChat
         messages={messages}
+        // calls function to update Firebase with new message
         onSend={messages => onSend(messages)}
         user={{
-          _id: 1,
+          _id: uid,
         }}
       />
     </View>
